5a699dd076b5d1d69ecac15a67158a75f80ce2ba
Dheeraj <kontham.dheeraj@gmail.com>
2019-12-01 22:43:52 +0530

    Updated StudentController response to have traceId in headers

--- src/main/java/com/projects/springbootsleuth/controller/StudentController.java
+++ src/main/java/com/projects/springbootsleuth/controller/StudentController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,21 +29,23 @@ public class StudentController {
 	
 	@GetMapping
 	public ResponseEntity<?> getStudents() {
-
 		LOGGER.info("Retrieving student details(sleuth service) ----- ");
 		List<StudentResponse> studentResponseList = null;
+		HttpHeaders headers =new HttpHeaders();
 		try {
 
 			studentResponseList = studentService.getStudents();
-
+			String traceId = MDC.get("X-B3-TraceId");
+			headers.set("traceId", traceId);
+			
 		} catch (Exception e) {
 
 			LOGGER.error("Unable to retrieve student details ------");
-			return new ResponseEntity<>(new StudentException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to retrieve student details",new Date()), HttpStatus.INTERNAL_SERVER_ERROR);
-
+			return new ResponseEntity<>(new StudentException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to retrieve student details",new Date()),headers, HttpStatus.INTERNAL_SERVER_ERROR);
+			
 		}
 		LOGGER.info("Retrieved student details(sleuth service) ----- ");
 		
-		return new ResponseEntity<>(studentResponseList, HttpStatus.OK);
+		return new ResponseEntity<>(studentResponseList,headers,HttpStatus.OK);
 	}
 }
