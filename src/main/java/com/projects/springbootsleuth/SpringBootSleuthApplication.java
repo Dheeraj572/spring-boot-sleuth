c0e69bef4887a269494e0bd00f03d5547e416608
Dheeraj <kontham.dheeraj@gmail.com>
2019-12-01 12:04:55 +0530

    Project setup

---
+++ src/main/java/com/projects/springbootsleuth/SpringBootSleuthApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springbootsleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSleuthApplication.class, args);
+	}
+
+}
